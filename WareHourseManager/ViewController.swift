b43f3ece1f7287311cb51fe115fe4f8a1b5796ff
Nguyen Thanh Dong <nguyenthanhdong0109@gmail.com>
2018-12-26 10:11:49 +0700

    Initial Commit

---
+++ WareHourseManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WareHourseManager
+//
+//  Created by Dong Nguyen Thanh on 12/26/18.
+//  Copyright © 2018 Dong Nguyen Thanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
